dd41016e4246e81af8bde5c5b026e9a2444ac885
ahmed kabil <ahmedskabil@gmail.com>
2026-03-17 03:04:38 +0200

    adding the Jenkinsfile

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 	}
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "index.html")
+		http.ServeFile(w, r, "static/index.html")
 	})
 
 	http.HandleFunc("/home", func(w http.ResponseWriter, r *http.Request) {
